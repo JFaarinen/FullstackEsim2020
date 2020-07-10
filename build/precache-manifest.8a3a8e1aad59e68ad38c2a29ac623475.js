9b88929ca71883838e25938d3f91fd63c9c0f7d0
Juho F <juho.faarinen@gmail.com>
2020-07-10 20:39:19 +0300

    Front Ja Back Nettiin

---
+++ build/precache-manifest.8a3a8e1aad59e68ad38c2a29ac623475.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d72501e5eeb263327719e183219775cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce72fbb8deb163b5be89",
+    "url": "/static/css/main.011b3ffd.chunk.css"
+  },
+  {
+    "revision": "bbabf0a26b8e88cac1c2",
+    "url": "/static/js/2.d6550153.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d6550153.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce72fbb8deb163b5be89",
+    "url": "/static/js/main.66f54d85.chunk.js"
+  },
+  {
+    "revision": "4b4367ca4fbadb01e2d9",
+    "url": "/static/js/runtime-main.e5a25448.js"
+  }
+]);
---
+++ build/precache-manifest.c4ae8b7b2365b145d3e2535e004b8f84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06bb17cb19589df2a50a55f625e8d6ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bae8a687d3a7e776cb08",
+    "url": "/static/css/main.011b3ffd.chunk.css"
+  },
+  {
+    "revision": "bbabf0a26b8e88cac1c2",
+    "url": "/static/js/2.d6550153.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d6550153.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bae8a687d3a7e776cb08",
+    "url": "/static/js/main.0449ceb0.chunk.js"
+  },
+  {
+    "revision": "4b4367ca4fbadb01e2d9",
+    "url": "/static/js/runtime-main.e5a25448.js"
+  }
+]);
